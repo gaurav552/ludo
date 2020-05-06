98fe825afc0db6b18b08cfb55aca002181c05e26
Gaurav Raj Ghimire <grg.grg.47@gmail.com>
2020-05-06 01:58:29 -0400

    fixes

--- scripts/game.js
+++ scripts/game.js
@@ -7,12 +7,12 @@ function broadcastRoll(message) {
 }
 
 function dice_roller(rand) {
-    rand = 6
+    // rand = 6
 
     move_used = false
         // console.log(rand)
         // let multi = localStorage.getItem("Multi") !== null ? JSON.parse(localStorage.getItem("Multi")).multi : []
-    if (rand != localStorage.getItem("Previous Roll")) {
+    if (rand != localStorage.getItem("Last Roll")) {
         if (rand == 1) {
             document.querySelector(".dice").style.transform = 'rotateY(360deg)'
         } else if (rand == 2) {
@@ -32,35 +32,36 @@ function dice_roller(rand) {
             document.querySelector(".dice").style.transform = 'rotateX(0deg)'
             setTimeout(() => {
                 document.querySelector(".dice").style.transform = "rotateY(360deg)"
-            }, 1000);
+            }, 1100);
         } else if (rand == 2) {
             document.querySelector(".dice").style.transform = 'rotateX(450deg)'
             setTimeout(() => {
                 document.querySelector(".dice").style.transform = "rotateY(-90deg)"
-            }, 1000);
+            }, 1100);
         } else if (rand == 3) {
             document.querySelector(".dice").style.transform = 'rotateX(450deg)'
             setTimeout(() => {
                 document.querySelector(".dice").style.transform = "rotateY(180deg)"
-            }, 1000);
+            }, 1100);
         } else if (rand == 4) {
             document.querySelector(".dice").style.transform = 'rotateX(450deg)'
             setTimeout(() => {
                 document.querySelector(".dice").style.transform = "rotateY(90deg)"
-            }, 1000);
+            }, 1100);
         } else if (rand == 5) {
             document.querySelector(".dice").style.transform = 'rotateX(360deg)'
             setTimeout(() => {
                 document.querySelector(".dice").style.transform = "rotateX(-90deg)"
-            }, 1000);
+            }, 1100);
         } else if (rand == 6) {
             document.querySelector(".dice").style.transform = 'rotateX(360deg)'
             setTimeout(() => {
                 document.querySelector(".dice").style.transform = "rotateX(90deg)"
-            }, 1000);
+            }, 1100);
         }
     }
     localStorage.setItem('Previous Roll', rand)
+    localStorage.setItem('Last Roll', rand)
 }
 
 
@@ -71,43 +72,72 @@ function dice_roller(rand) {
 //     })
 // })
 
-function mover(e){
+function mover(e) {
     // console.log("appks")
     if (localStorage.getItem("Color") == localStorage.getItem('Turn')) {
         // console.log(localStorage.getItem("Previous Roll"))
         if (localStorage.getItem("Previous Roll") != null) {
-            console.log(e.target.parentElement.getAttribute("this-path"))
-            if(e.target.parentElement.hasAttribute("this-path")){
+            // console.log(e.target.parentElement.getAttribute("this-path"))
+            if (e.target.parentElement.hasAttribute("this-path")) {
                 let my_tile = (e.target.parentElement.getAttribute("this-path") | 0)
-                let new_tile = (my_tile + (localStorage.getItem("Previous Roll") | 0) )
+                let new_tile = (my_tile + (localStorage.getItem("Previous Roll") | 0))
 
-                let op =getTemplate("out_piece_template")
-                op.querySelector("button").classList.add(localStorage.getItem("Color")+"_piece")
+                let op = getTemplate("out_piece_template")
+                op.querySelector("button").classList.add(localStorage.getItem("Color") + "_piece")
 
-                if(new_tile <= 51){
-                    document.querySelector("[this-path='"+ new_tile +"']").appendChild(op)
-                } else{
-                    new_tile = new_tile-51
-                    document.querySelector("."+ localStorage.getItem("Color")+"_safe[safe-path='"+ new_tile +"']").appendChild(op)
+                if (new_tile <= 51) {
+                    e.target.remove()
+                    document.querySelector("[this-path='" + new_tile + "']").appendChild(op)
+                    localStorage.removeItem("Previous Roll")
+                } else {
+
+                    new_tile = new_tile - 51
+                    completion(new_tile,e)
                 }
 
-                e.target.remove()
+            } else if(e.target.parentElement.hasAttribute("safe-path")){
+                let my_tile = (e.target.parentElement.getAttribute("safe-path") | 0)
+                let new_tile = (my_tile + (localStorage.getItem("Previous Roll") | 0))
+                completion(new_tile, e)
             }
+
+            // if(localStorage.getItem("Previous Roll") == 6){
+            document.querySelector(".roll>button").disabled = false
+
             e.target.disabled = false
         }
     }
 }
 
-function move_other(){
+
+function completion(new_tile, e){
+
+    let op = getTemplate("out_piece_template")
+    op.querySelector("button").classList.add(localStorage.getItem("Color") + "_piece")
+
+    if(new_tile == 6){
+        e.target.remove()
+        localStorage.removeItem("Previous Roll")
+    } else if(new_tile < 6){
+        e.target.remove()
+        document.querySelector("." + localStorage.getItem("Color") + "_safe[safe-path='" + new_tile + "']").appendChild(op)
+        localStorage.removeItem("Previous Roll")
+    } else if(new_tile > 6){
+
+    }
+}
+
+
+function move_other() {
 
 }
 
-function other_out(response){
+function other_out(response) {
     let from = response.from
     let to = response.to
     document.querySelectorAll(from)[0].disabled = true
-    let op =getTemplate("out_piece_template")
-    op.querySelector("button").classList.add(response.color+"_piece")
-    document.querySelector("[uni-path='"+ to+"']").appendChild(op)
-    // console.log(response)
+    let op = getTemplate("out_piece_template")
+    op.querySelector("button").classList.add(response.color + "_piece")
+    document.querySelector("[uni-path='" + to + "']").appendChild(op)
+        // console.log(response)
 }
